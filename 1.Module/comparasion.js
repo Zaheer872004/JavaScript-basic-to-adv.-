0513279d29d543b967983f237e027b9d7f8d0dc9
Zaheer872004 <129886416+Zaheer872004@users.noreply.github.com>
2023-08-05 07:24:16 +0000

    comparasion

---
+++ 1.Module/comparasion.js
@@ -0,0 +1,38 @@
+/******* Comparasion Operator*******/
+//  >,<,==,<=,>=,!= ---> only return true or false
+//avoid this type comparasion
+
+
+// console.log(null > 0); // null set as 0 then compare 0 > 0 --> return false
+//avoid this type comparasion
+
+
+// console.log(null == 0); // here is compare the value without null set 0
+//avoid this type comparasion
+
+
+// console.log(null >= 0); // null set as 0 and then compare
+
+
+// In javaScript equality and comparation both are different
+
+// console.log(undefined > 0);   // avoid this type comparasion
+// console.log(undefined == 0); //avoid this type comparasion
+// console.log(undefined < 0);     //avoid this type comparasion
+
+//  ==  --> compare the value 
+
+console.log("2" == 2);  
+console.log("2" == "2");
+console.log(2 == "2");
+        // above ex only check value and compare it
+
+// === --> compare the value as well as datatype also check
+ 
+console.log("2" === 2);
+console.log(2 === "2");
+console.log(2 === 2);
+console.log(2 === 3);
+        // above ex check value and datatype also. 
+
+
