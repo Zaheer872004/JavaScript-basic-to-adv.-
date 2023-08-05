bdd46f27291f88c3f07f6744b7b2996bfa69ad95
Zaheer872004 <129886416+Zaheer872004@users.noreply.github.com>
2023-08-05 06:54:44 +0000

    datatype-conversion

--- 1.Module/datatype_conversion.js
+++ 1.Module/datatype_conversion.js
@@ -8,13 +8,13 @@ let score = "Zaheer"
 
 
 
-console.log(typeof score);
-console.log(typeof (score))
+// console.log(typeof score);
+// console.log(typeof (score))
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 
@@ -35,8 +35,8 @@ let isloggedIn = 33
 
 let booleanIsLoggedIn =Boolean(isloggedIn)
 
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 // 1 -> true ; 0 -> false
@@ -47,25 +47,44 @@ console.log(booleanIsLoggedIn);
 
 let value = 123
 
-console.log(typeof value);
-console.log(value);
+// console.log(typeof value);
+// console.log(value);
 
 
 let changeValue = String(value)
 
-console.log(typeof changeValue);
-console.log(changeValue);
+// console.log(typeof changeValue);
+//console.log(changeValue);
 
 
+            /*************Operation***********/
+
+ let value1 = 3
+let negvalue = -value1
+//  console.log(negvalue);
 
 
+//  console.log(2+2);
+//  console.log(2**4);  //2 to the power 4
+ // other is similar to famous one
+
+ let str1 = "hello long time not seen you "
+ let str2 = "Zaheer"
+ let str3 = str1 + str2
+//  console.log(str3);
 
 
+// what happen when I'm write this things
+
+console.log("1"+ 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // when I'm write first string then after all no. convert into string
+console.log(1 + 2 + "2"); // first 1 and 2 add then cocatenate(join) into string become 32
 
 
-
-
-
+// Alt + shift + down_arrow ---> to copy and paste in down line like this
+// console.log();
+// console.log();
 
 
 
