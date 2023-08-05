e7ac5ea959568b6f342d46465bfd5da24e2e7d27
Zaheer872004 <129886416+Zaheer872004@users.noreply.github.com>
2023-08-05 06:19:34 +0000

    datatype-conversion

---
+++ 1.Module/datatype_conversion.js
@@ -0,0 +1,84 @@
+// let score = 33 
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Zaheer"
+
+
+
+console.log(typeof score);
+console.log(typeof (score))
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+/*
+//"33" -> 33
+// "33abc" -> NaN
+// true -> 1; false -> 0
+
+*/
+
+
+
+
+// let isloggedIn = 1
+// let isloggedIn = "Zaheer"
+// let isloggedIn = null
+let isloggedIn = 33
+
+let booleanIsLoggedIn =Boolean(isloggedIn)
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+// 1 -> true ; 0 -> false
+"" --> false 
+"zaheer" -> true
+*/
+
+
+let value = 123
+
+console.log(typeof value);
+console.log(value);
+
+
+let changeValue = String(value)
+
+console.log(typeof changeValue);
+console.log(changeValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
